6f70213504eea3a29fe128512fd763d0a5180946
Dylan van der Ven <dylanislang@gmail.com>
2024-01-09 10:03:10 +0100

    update ???

--- src/bestellingdoorvoeren.php
+++ src/bestellingdoorvoeren.php
@@ -1,17 +1,29 @@
 <?php
 
+namespace Acme;
 use Acme\classes\Bestelling;
 
 require "../vendor/autoload.php";
 
 if ($idTafel = $_POST['idtafel'] ?? false) {
-
+    $b = new Bestelling(((int)$idTafel));
+    $producten = [];
     // TODO: De bestelling doorvoeren in de database (maak gebruik van de Bestelling class)
+// var_dump($_POST['products']);
+foreach($_POST["products"] as $product){
+    $numberOfProducts = $_POST["products"] ?? 0;
 
+}
+echo "<br><br><br>";
+var_dump($product);
+
+// $bestelling->addProducts($producten);
+// $bestelling->saveBestelling();
 } else {
     http_response_code(404);
     include('error_404.php');
     die();
 }
 
-header("Location: index.php");
+
+// header("Location: index.php");

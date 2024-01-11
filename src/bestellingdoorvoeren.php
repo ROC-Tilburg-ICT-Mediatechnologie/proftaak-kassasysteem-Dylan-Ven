70967352e31a7cba0da798fc3a0703ca7f046b1d
Dylan van der Ven <dylanislang@gmail.com>
2024-01-11 09:20:02 +0100

    updated bestellingdoorvoeren

--- src/bestellingdoorvoeren.php
+++ src/bestellingdoorvoeren.php
@@ -1,17 +1,37 @@
 <?php
 
+namespace Acme;
 use Acme\classes\Bestelling;
 
 require "../vendor/autoload.php";
 
 if ($idTafel = $_POST['idtafel'] ?? false) {
-
+    $b = new Bestelling(((int)$idTafel));
     // TODO: De bestelling doorvoeren in de database (maak gebruik van de Bestelling class)
+    $order = $_POST['products'];
+    //deze Function is om de array te checken of er alleen maar nummers in de array staan
+    function containsOnlyNumbers($order) {
+        foreach ($order as $value) {
+            if (!preg_match('/^\d+$/', $value)) {
+                return false;
+            }
+        }
+        return true;
+    }
+     if (containsOnlyNumbers($order)) {
+        $b->addProducts($order);
+        $b->saveBestelling();
+        echo "Order successful";
+    } else {
+        echo "Order canceled";
+    }
+    
 
 } else {
     http_response_code(404);
     include('error_404.php');
     die();
 }
+
 
 header("Location: index.php");
